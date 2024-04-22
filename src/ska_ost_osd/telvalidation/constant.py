d77d698e627927c8a73e5aa4f00204fdf2c8bfc0
Manish Lalwani <manish_lalwani11@persistent.com>
2024-04-22 12:55:17 +0530

    NAK-849 Fix linting issue

--- src/ska_ost_osd/telvalidation/constant.py
+++ src/ska_ost_osd/telvalidation/constant.py
@@ -20,8 +20,6 @@ SKA_LOW_TELESCOPE = "low"
 SKA_MID_TELESCOPE = "mid"
 SKA_SBD = "sbd"
 
-CAR_TELMODEL_SOURCE = (
-    "car:ost/ska-ost-osd?main",
-)
+CAR_TELMODEL_SOURCE = ("car:ost/ska-ost-osd?main",)
 
 INTERFACE_PATTERN = r"^https://schema\.skao\.int/[a-zA-Z-]+/\d+\.\d+$"
