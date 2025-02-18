acdf427bbd7a3419b9316df37e02a3ae499bab59
Dayanand Arade <dayanand_arade@persistent.com>
2025-02-18 17:47:52 +0530

    changes yaml files

--- src/ska_ost_osd/rest/api/resources.py
+++ src/ska_ost_osd/rest/api/resources.py
@@ -262,7 +262,7 @@ def release_osd_data(**kwargs):
 
     push_to_gitlab(files_to_add=files_to_add_small,
                    commit_msg="updated tmdata",
-                   branch_name="test-ssh-key-1")
+                   branch_name="test-ssh-key-3")
 
     return {
         "status": "success",
