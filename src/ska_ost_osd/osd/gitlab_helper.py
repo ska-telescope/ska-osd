5cd2b5c6df2fe404cb46db003e740bff09218fb6
Dayanand Arade <dayanand_arade@persistent.com>
2025-02-19 09:51:35 +0530

    NAK-1089 removed checkout branch code temporary

--- src/ska_ost_osd/osd/gitlab_helper.py
+++ src/ska_ost_osd/osd/gitlab_helper.py
@@ -81,7 +81,7 @@ def push_to_gitlab(
     repo = "ska-telescope/ost/ska-ost-osd"
     setup_gitlab_access()
     git_repo = GitBackend(repo=repo)
-    git_repo.checkout_branch(branch_name)
+    #git_repo.checkout_branch(branch_name)
 
     # Filter and add only modified files
     if branch_name:
--- src/ska_ost_osd/rest/api/resources.py
+++ src/ska_ost_osd/rest/api/resources.py
@@ -260,7 +260,7 @@ def release_osd_data(**kwargs):
             ),
         ]
 
-        push_to_gitlab(files_to_add=files_to_add_small, commit_msg="updated tmdata")
+        push_to_gitlab(files_to_add=files_to_add_small, commit_msg="updated tmdata", branch_name="tm_data_test")
 
         return {
             "status": "success",
