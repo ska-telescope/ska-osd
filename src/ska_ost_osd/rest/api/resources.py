fca1f02c7bed9e2b7b0b91f63d5878fba542f4eb
Dayanand Arade <dayanand_arade@persistent.com>
2025-02-21 14:44:53 +0530

    NAK-1089 updated new repo creation

--- src/ska_ost_osd/rest/api/resources.py
+++ src/ska_ost_osd/rest/api/resources.py
@@ -254,6 +254,7 @@ def release_osd_data(**kwargs):
         push_to_gitlab(
             files_to_add=files_to_add_small,
             commit_msg="updated tmdata",
+            branch_name="nak-1089-tmdata-artefact-release"
         )
 
         return {
