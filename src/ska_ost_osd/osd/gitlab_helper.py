c4e8759817af46bc9fceeedb99a7ea51fb336778
Dayanand Arade <dayanand_arade@persistent.com>
2025-02-21 21:59:57 +0530

    NAK-1089 commit transaction

--- src/ska_ost_osd/osd/gitlab_helper.py
+++ src/ska_ost_osd/osd/gitlab_helper.py
@@ -104,7 +104,7 @@ def push_to_gitlab(
             except ValueError as err:
                 if str(err) == "Branch Already Exists":
                     logger.error("Branch already exists, try a different branch name")
-                    #git_repo.checkout_branch(branch_name)
+                    git_repo.checkout_branch(branch_name)
                     logger.error("Cheking out same branch")
 
         modified_files = []
