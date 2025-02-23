0dbdc3853cb605fc42ccdec80167419c9efb0edb
Dayanand Arade <dayanand_arade@persistent.com>
2025-02-23 13:21:30 +0530

    NAK-1089 fixed path issue

--- src/ska_ost_osd/osd/new_data_backend.py
+++ src/ska_ost_osd/osd/new_data_backend.py
@@ -337,8 +337,9 @@ class GitBackend(NewDataBackend):
 
         with open(path, encoding="utf-8") as f:
             logger.info("file content in _add method %s", f.read())
-        if "mid_capabilities.json" in path:
-            path = 'tmdata/ska1_mid/mid_capabilities.json'
+        if "mid_capabilities.json" == path.name:
+            from pathlib import Path
+            path = Path('tmdata/ska1_mid/mid_capabilities.json')
         self._repo.index.add([path])
 
     def _commit(self, message: str):
