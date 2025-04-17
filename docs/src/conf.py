4e676222fe453f952c47f28db10e263ec48cc815
Betelgeusenova <sagar_wankhede1@persistent.com>
2025-04-17 14:23:29 +0530

    NAK-1266 made changes

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -22,6 +22,9 @@ import sys
 sys.path.insert(0, os.path.abspath("../../src"))
 autodoc_mock_imports = ["astropy", "gitlab", "simpleeval", "ska_telmodel", "pydantic", "prance"]
 
+
+from ska_ost_osd.osd import osd
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
