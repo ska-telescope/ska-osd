0f183fbd524f8717817e6293ab7bb5f627701d46
Yuvraj Kale <yuvraj_kale@persistent.com>
2025-07-15 18:39:57 +0530

    NAK-1313 - removed televalidation messages module

--- src/ska_ost_osd/telvalidation/common/telvalidation_messages.py
+++
@@ -1,3 +0,0 @@
-# validation msgs
-SEMANTIC_VALIDATION_DISABLED_MSG = "Semantic Validation is currently disable"
-SEMANTICALLY_VALID_JSON_MSG = "JSON is semantically valid"
--- src/ska_ost_osd/telvalidation/routers/api.py
+++ src/ska_ost_osd/telvalidation/routers/api.py
@@ -13,12 +13,10 @@ from ska_ost_osd.common.utils import (
 from ska_ost_osd.osd.routers.api import handle_validation_error, osd_router
 from ska_ost_osd.telvalidation.common.constant import (
     CAR_TELMODEL_SOURCE,
-    SEMANTIC_VALIDATION_VALUE,
-    SWAGGER_SEMANTIC_VALIDATION_JSON_FILE_PATH,
-)
-from ska_ost_osd.telvalidation.common.telvalidation_messages import (
     SEMANTIC_VALIDATION_DISABLED_MSG,
+    SEMANTIC_VALIDATION_VALUE,
     SEMANTICALLY_VALID_JSON_MSG,
+    SWAGGER_SEMANTIC_VALIDATION_JSON_FILE_PATH,
 )
 from ska_ost_osd.telvalidation.models.semantic_schema_validator import (
     SemanticValidationModel,
