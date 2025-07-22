68d0f2ab7b9e35a23f0f61e0e7d9cc96389c7b22
Yuvraj Kale <yuvrajkale.ska@gmail.com>
2025-07-22 08:33:06 +0000

    NAK-1313 - Semantic validation sample file path updates

--- src/ska_ost_osd/telvalidation/common/constant.py
+++ src/ska_ost_osd/telvalidation/common/constant.py
@@ -15,7 +15,7 @@ LOW_SBD_VALIDATION_CONSTANT_JSON_FILE_PATH = (
     "instrument/scheduling-block/validation/low_sbd-validation-constants.json"
 )
 SWAGGER_SEMANTIC_VALIDATION_JSON_FILE_PATH = (
-    "src/ska_ost_osd/telvalidation/data/sample_semantic_validation.json"
+    "telvalidation/data/sample_semantic_validation.json"
 )
 MID_LAYOUT_CONSTANT_JSON_FILE_PATH = "instrument/ska1_mid/layout/mid-layout.json"
 LOW_LAYOUT_CONSTANT_JSON_FILE_PATH = "instrument/ska1_low/layout/low-layout.json"
