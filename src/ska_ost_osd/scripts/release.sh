78806c698eb14d7eca55c53e6f5af3f4eed7d5af
Yuvraj Kale <yuvraj_kale@persistent.com>
2025-07-23 17:02:45 +0530

    NAK-1313 - updated release.sh file

--- src/ska_ost_osd/scripts/release.sh
+++ src/ska_ost_osd/scripts/release.sh
@@ -2,8 +2,8 @@
 
 base_path=$(dirname "$(readlink -f "release.sh")")
 
-observatory_file_location="$base_path/tmdata/observatory_policies.json"
-version_mapping_file_location="$base_path/tmdata/version_mapping/cycle_gitlab_release_version_mapping.json"
+observatory_file_location="tmdata/observatory_policies.json"
+version_mapping_file_location="tmdata/version_mapping/cycle_gitlab_release_version_mapping.json"
 
 function CheckFileExists (){
 
